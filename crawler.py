bb3f62e2ac27e626beee403214e2aa9cb2af75ea
timothy.lee <marlboromoo@gmail.com>
2013-05-27 13:23:29 +0800

    Ability to search/show/edit local cache data.

--- crawler.py
+++ crawler.py
@@ -121,6 +121,13 @@ class Crawler(object):
         """
         return unicode(string).encode('unicode_escape')
 
+    def safe_unicode(self, obj):
+        """docstring for safe_unicode"""
+        try:
+            return obj.decode('utf8')
+        except Exception:
+            return unicode(obj)
+
     def pickle_dump(self):
         """docstring for pickle_dump"""
         pickle.dump(self.items, open(self.pickle_path, 'wb'))
@@ -132,19 +139,27 @@ class Crawler(object):
         except Exception:
             self.items = {}
             print 'No existing pickle file!'
+
+    def __fetch(self, id_):
+        """docstring for __fetch"""
+        context = self.get_string_context(id_)
+        string, translation = self.get_string_translation(id_)
+        return context, string, translation
     
     def fetch(self, resume=True):
         """fetch data"""
         ids = self.ids = self.get_string_ids()
         i = 1
+        print ids
         for id_ in ids:
             if resume:
                 #. don't fetch data if exist
                 if id_ in self.items.keys():
                     i += 1
                     continue
-            context = self.get_string_context(id_)
-            string, translation = self.get_string_translation(id_)
+            #context = self.get_string_context(id_)
+            #string, translation = self.get_string_translation(id_)
+            context, string, translation = self.__fetch(id_)
             if self.verbose:
                 print "[%s/%s] %s=%s" % (i, len(ids), context, translation)
             self.items[id_] = {
@@ -165,34 +180,118 @@ class Crawler(object):
                 for k,v in self.items.items():
                     f.write("%s=%s\n" % (
                         v['context'], self.unicode_byte_string(v['translation'])))
-        except Exception, e:
-            raise e
+        except Exception:
+            print "Make fail."
+
+    def __show_item(self, id_, attrs, short=True):
+        """swho pretty item"""
+        print "%s" % ('='*80)
+        print "Id: %s Context: %s" % (id_, attrs['context'])
+        string = "%s ..." % attrs['string'][:26] if short else attrs['string']
+        tran = "%s ..." % attrs['translation'][:26] if short else attrs['translation']
+        print "String: %s\nTranslation: %s" % (string, tran)
+
+    def __show_summary(self):
+        """docstring for __show_summary"""
+        pass
+
+    def show_item_by_id(self, id_):
+        """show item by id"""
+        if id_ in self.items:
+            self.__show_item(id_, self.items[id_], short=False)
+
+    def get_id_by_context(self, context):
+        """get id by context"""
+        for k,v in self.items.items():
+            if context == v['context']:
+                return k
+
+    def show_item_by_context(self, context):
+        """docstring for show_item_by_context"""
+        id_ = self.get_id_by_context(context)
+        if id_:
+            return self.show_item_by_id(id_)
 
     def list_items(self):
-        """docstring for """
-        print self.items
+        """list all items"""
         for k,v in self.items.items():
-            print "id:%s context:%s" % (k, v['context'])
-            print "string:%s\ntranslation:%s" % (v['string'], v['translation'])
+            self.__show_item(k, v)
+
+    def search_item(self, keyword):
+        """search item by keyword"""
+        keyword = keyword.lower()
+        result = {}
+        for k,v in self.items.items():
+            if keyword in v['string']:
+                result[k] = v
+        for k,v in result.items():
+            self.__show_item(k,v)
+
+    def edit_item_by_id(self, id_, translation):
+        """edit translation for specific id"""
+        if id_ in self.items:
+            self.items[id_]['translation'] = self.safe_unicode(translation)
+        self.pickle_dump()
+
+    def edit_item_by_context(self, context, translation):
+        """edit translation for specific context"""
+        id_ = self.get_id_by_context(context)
+        if id_:
+            return self.edit_item_by_id(id_)
+
+    def update(self, id_):
+        """docstring for update"""
+        pass
 
 def main():
     """docstring for """
 parser = argparse.ArgumentParser()                                          
-#. commands
-subparsers = parser.add_subparsers(dest='cmd', title='command', help='valid commands')
-parser_fetch = subparsers.add_parser('fetch', help="fetch data from getlocalization.com.")
-parser_list = subparsers.add_parser('list', help="list data from local cache.")
-parser_make = subparsers.add_parser('make', help="generate file with specify project type.")
-parser_make.add_argument('--type', default='java', choices=['java', '..'])
+subparsers = parser.add_subparsers(
+    dest='cmd', title='command')
+#. cmd: fetch
+parser_fetch = subparsers.add_parser(
+    'fetch', help="fetch data from getlocalization.com")
+#. cmd: list
+parser_list = subparsers.add_parser(
+    'list', help="list data from local cache")
+#. cmd: make
+parser_make = subparsers.add_parser(
+    'make', help="generate file with specific type from local cache")
+parser_make.add_argument(
+    'path', help="path to writing file")
+parser_make.add_argument(
+    '--type', default='java', choices=['java', '..'])
+#. cmd: search
+parser_search = subparsers.add_parser(
+    'search', help="search item from local cache")
+parser_search.add_argument(
+    'keyword', help="keyword to search")
+#. cmd: show
+parser_show = subparsers.add_parser(
+    'show', help="show the item from local cache")
+#. cmd: edit
+parser_edit = subparsers.add_parser(
+    'edit', help="edit the translation in the local cache")
+parser_edit.add_argument(
+    'translation', help="new translation to update")
 #. global args
-parser.add_argument('--project', default='mcmmo', help="project to crawl, default: mcmmo.")
-parser.add_argument('--lang', default='zh-TW', help="language to crawl, default: zh-TW")
-parser.add_argument('--no_reume', action='store_true', help="no resuming download.")
-parser.add_argument('--no_verbose', action='store_true', help="no verbose output.")
+group_target= parser.add_mutually_exclusive_group()
+group_target.add_argument(
+    '--id', help="specify the id")
+group_target.add_argument(
+    '--context', help="specify the context")
+parser.add_argument(
+    '--project', default='mcmmo', help="project to crawl, default: mcmmo")
+parser.add_argument(
+    '--lang', default='zh-TW', help="language to crawl, default: zh-TW")
+parser.add_argument(
+    '--no_resume', action='store_true', help="no resuming download")
+parser.add_argument(
+    '--no_verbose', action='store_true', help="no verbose output")
 #. parse args
 args = parser.parse_args()     
 verbose = False if args.no_verbose else True
-resume = False if args.no_reume else True
+resume = False if args.no_resume else True
 #. do my job
 crawler = Crawler(project=args.project, language=args.lang)
 if args.cmd == 'fetch':
@@ -200,8 +299,25 @@ if args.cmd == 'fetch':
 if args.cmd == 'list':
     crawler.list_items()
 if args.cmd == 'make':
-    path='/tmp/zh_TW.locale'
-    crawler.make_java_properties(path)
+    if args.type == 'java':
+        crawler.make_java_properties(args.path)
+if args.cmd == 'search':
+    crawler.search_item(args.keyword)
+target_err_msg = "Use with option --id or --context"
+if args.cmd == 'show':
+    if args.id:
+        crawler.show_item_by_id(args.id)
+    elif args.context:
+        crawler.show_item_by_context(args.context)
+    else:
+        print "%s" % (target_err_msg)
+if args.cmd == 'edit':
+    if args.id:
+        crawler.edit_item_by_id(args.id, args.translation)
+    elif args.context:
+        crawler.edit_item_by_context(args.id, args.translation)
+    else:
+        print "%s" % (target_err_msg)
 
 if __name__ == '__main__':
     main()
